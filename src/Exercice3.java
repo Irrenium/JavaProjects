eb2362bf4c67fef3b8cb8d4a7f29233a316609c4
Reyad <riadox41@gmail.com>
2023-02-17 00:33:48 +0100

    Updated Exercice3 file

--- src/Exercice3.java
+++ src/Exercice3.java
@@ -1,3 +1,30 @@
-public class Exercice3 {
 
+public class Exercice3 {
+    /**
+     * 
+     * @param n chane de caratère à analyser
+     * @return
+     */
+    public String getChaineDesNombresDe1aN(String n) {
+        if (n == null) {
+            System.out.println("La chaîne est null!");
+            return "";
+        }
+
+        if (n.length() < 0) {
+            System.out.println("La chaîne est vide!");
+            return "";
+        }
+
+        StringBuilder chaine = new StringBuilder("");
+        int nombre = Integer.parseInt(n);
+        if (nombre < 0) {
+            System.out.println("Entrez un nombre positif!");
+        }
+
+        for (int i = 0; i < nombre; i++) {
+            chaine.append(i);
+        }
+        return chaine.toString();
+    }
 }
