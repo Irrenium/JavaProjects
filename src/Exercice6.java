ecc56b1eee5a7f1eb9f7eb4a3d48592c0a0a0ebd
Reyad <riadox41@gmail.com>
2023-02-18 01:59:59 +0100

    Updated Exercice6 file

--- src/Exercice6.java
+++ src/Exercice6.java
@@ -2,6 +2,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Exercice6 {
+
+    /**
+     * Une Methode qui permet :
+     * - Le tirage aléatoire d’un entier appartenant à l’intervalle [0, n [, sachant
+     * que n est déjà donnée.
+     * 
+     * - Permet à l’utilisateur de faire des propositions pour deviner ce nombre; à
+     * chaque proposition le joueur sera informé que le nombre qu’il a donné est «
+     * trop grand » ou « trop petit ».
+     * 
+     * - À la fin du jeu, si le joueur trouve le nombre mystérieux, le nombre
+     * d’essais nécessaires est affiché.
+     * 
+     * - Le nombre max d’essais autorisés est 10 propositions. Dans le cas où le
+     * joueur ne découvre pas le nombre mystérieux au bout des 10 essais, la
+     * solution lui sera donnée
+     */
     public void jeu() {
         System.out.println("donner un nombre");
         Scanner entree = new Scanner(System.in);
